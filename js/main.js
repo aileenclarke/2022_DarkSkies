85530f912693191ca236269129479d5f3491e733
aileenclarke <56174664+aileenclarke@users.noreply.github.com>
2022-04-24 17:31:13 -0500

    Aileen Edits 4/24
    Set up side-by-side.js extension.
    Added Mapbox base maps.
    Added flyTo capabilities in button form.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,44 @@
 //insert code here!
+
+// leaflet side-by-side
+var map;
+
+function createMap(){
+    /*
+    map = L.map('map', {
+        center:[39.83,-98.58],
+        zoom: 4
+    });
+    */
+    map = L.map('map').setView([39,-98],4);
+
+    // mapbox layer 1
+    var layer1 = L.tileLayer('https://api.mapbox.com/styles/v1/amclarke2/cl0g3m8oh000h14n0ok1oan43/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYW1jbGFya2UyIiwiYSI6ImNrczZtNjkwdjAwZngycW56YW56cHUxaGsifQ._Cc2V5nKC5p2zfrYqw7Aww', { 
+        attribution: '&copy <a href="https://www.mapbox.com/map-feedback/">Mapbox</a> &copy <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+    // mapbox layer 2
+    var layer2 = L.tileLayer('https:///api.mapbox.com/styles/v1/amclarke2/cl2dhi5fc003q14o3jz96d8dp/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYW1jbGFya2UyIiwiYSI6ImNrczZtNjkwdjAwZngycW56YW56cHUxaGsifQ._Cc2V5nKC5p2zfrYqw7Aww', { 
+        attribution: '&copy <a href="https://www.mapbox.com/map-feedback/">Mapbox</a> &copy <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+    // compare two layers on map
+    L.control.sideBySide(layer1, layer2).addTo(map);
+};
+
+document.getElementById("buttonND").addEventListener("click", function () {
+    map.flyTo([48.15, -103.62], 10, {
+        animate: true,
+        duration: 2 // in seconds
+    });
+});
+
+document.getElementById("buttonTX").addEventListener("click", function () {
+    map.flyTo([30.27, -97.74], 10, {
+        animate: true,
+        duration: 2 // in seconds
+    });
+});
+
+https://api.mapbox.com/styles/v1/amclarke2/cl2dhi5fc003q14o3jz96d8dp.html?title=view&access_token=pk.eyJ1IjoiYW1jbGFya2UyIiwiYSI6ImNrczZtNjkwdjAwZngycW56YW56cHUxaGsifQ._Cc2V5nKC5p2zfrYqw7Aww&zoomwheel=true&fresh=true#9/37.78/-122.4241
+document.addEventListener('DOMContentLoaded',createMap)
