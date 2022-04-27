85a9166272e62aa6af88104f189debb7319ea2aa
aileenclarke <56174664+aileenclarke@users.noreply.github.com>
2022-04-27 16:48:16 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,43 +1,43 @@
 //insert code here!
 
 // leaflet side-by-side
-var map;
+var sliderMap;
 
-function createMap(){
+function createSliderMap(){
     /*
     map = L.map('map', {
         center:[39.83,-98.58],
         zoom: 4
     });
     */
-    map = L.map('map',{
+    sliderMap = L.map('sliderMap',{
         scrollWheelZoom: false})
         .setView([39,-98],4);
 
     // mapbox layer 1
     var layer1 = L.tileLayer('https://api.mapbox.com/styles/v1/amclarke2/cl0g3m8oh000h14n0ok1oan43/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYW1jbGFya2UyIiwiYSI6ImNrczZtNjkwdjAwZngycW56YW56cHUxaGsifQ._Cc2V5nKC5p2zfrYqw7Aww', { 
         attribution: '&copy <a href="https://www.mapbox.com/map-feedback/">Mapbox</a> &copy <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
+    }).addTo(sliderMap);
 
     // mapbox layer 2
     var layer2 = L.tileLayer('https:///api.mapbox.com/styles/v1/amclarke2/cl2dhi5fc003q14o3jz96d8dp/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYW1jbGFya2UyIiwiYSI6ImNrczZtNjkwdjAwZngycW56YW56cHUxaGsifQ._Cc2V5nKC5p2zfrYqw7Aww', { 
         attribution: '&copy <a href="https://www.mapbox.com/map-feedback/">Mapbox</a> &copy <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
+    }).addTo(sliderMap);
 
     // compare two layers on map
-    L.control.sideBySide(layer1, layer2).addTo(map);
+    L.control.sideBySide(layer1, layer2).addTo(sliderMap);
 };
 
 
 document.getElementById("buttonND").addEventListener("click", function () {
-    map.flyTo([48.15, -103.62], 10, {
+    sliderMap.flyTo([48.15, -103.62], 10, {
         animate: true,
         duration: 2 // in seconds
     });
 });
 
 document.getElementById("buttonTX").addEventListener("click", function () {
-    map.flyTo([30.27, -97.74], 10, {
+    sliderMap.flyTo([30.27, -97.74], 10, {
         animate: true,
         duration: 2 // in seconds
     });
@@ -56,13 +56,13 @@ function isInPosition(){
     y = rect.top;
     w = rect.width;
     h = rect.height;
-    console.log(y)
-    console.log(innerHeight)
+    //console.log(y)
+    //console.log(innerHeight)
     var position1 = y / window.innerHeight
-    console.log(position1)
+    //console.log(position1)
 
     if (position1 >= .4 && position1 <= .42){
-        map.flyTo([48.15, -103.62], 10, {
+        sliderMap.flyTo([48.15, -103.62], 10, {
             animate: true,
             duration: 2 // in seconds
         });
@@ -85,5 +85,5 @@ function scroll(){
 
 
 
-document.addEventListener('DOMContentLoaded',createMap)
-document.addEventListener('scroll',isInPosition)
+document.addEventListener('DOMContentLoaded', createSliderMap)
+document.addEventListener('scroll', isInPosition)
